b91201730b971418aded26c72c0d894f4b71c09a
hua638 <1321496817@qq.com>
2019-12-24 12:28:38 +0800

    ''

---
+++ src/views/learn1.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="learn1">
+   <ul>
+     <li>手机：13552508741</li>
+     <li>邮箱：1321496817@qq.com</li>
+     <li>学校：北京财经专修学院</li>
+   </ul>
+   <div class="box">
+     自我评价： 1.熟悉 W3C 标准，擅长 HTML5、CSS3、JavaScript 前端开发及移动端开发，根据设 UI 计图进行快速布局，解
+决浏览器兼容性问题，
+独立建站。
+2.熟练掌握 jQuery 及 jQuery 插件，有良好的编码、沟通协作能力和学习能力。
+3.熟悉 ES6 语法和 Vue、React 等前端主流开发框架，熟练使用脚手架，对 Vue 有着项目开发经验。
+4.对微信小程序有着一定的了解，能够独立完成小程序 WXML 页面、WXSS 样式开发与调整，满足产品设计效
+果。
+5.擅长 Ajax、JSONP、Axios、微信交互等前后台交互技术，有着实际的项目开发经验。
+6.熟练使用 BootStrap、Element-UI、Swiper 等 UI 框架，快速实现布局效果。
+7.熟悉 Node.JS，可以使用 Express 创建项目，对 MySQL 进行数据处理，熟练掌握 NPM、CNPM、Bower 包
+管理器。
+8.熟练使用 Git 版本控制工具，具有丰富的团队开发经验、较强的学习能力、良好的编码习惯。
+9.熟练使用 Sublime、HBuilder、VScode 等常用的前端编程软件，能够快速学习新的编程软件。
+   </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+*{
+	margin:0;
+	padding: 0;
+	list-style: none;
+	text-decoration: none;
+}
+.learn1{
+  width: 100%;
+  height: auto;
+  
+}
+.learn1 ul{
+  width: 100%;
+  height: 52px;
+  display: flex;
+}
+.learn1 ul li{
+  flex: 1;
+  text-align: center;
+  line-height: 52px;
+}
+.box{
+  width: 100%;
+  padding: 0 30px;
+  line-height: 40px;
+}
+</style>
